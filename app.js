76b3a5f28075867594d4a5de7af6fc1d82e53ddd
gordela <gordogordel@gmail.com>
2019-05-09 21:34:49 +0400

    Login done

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+//Passport Config
+require("./config/passport")(passport);
 
 // DB Config
 const db = require("./config/keys").mongoURI;
@@ -33,6 +37,10 @@ app.use(
   })
 );
 
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash
 app.use(flash());
 
@@ -40,6 +48,7 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
